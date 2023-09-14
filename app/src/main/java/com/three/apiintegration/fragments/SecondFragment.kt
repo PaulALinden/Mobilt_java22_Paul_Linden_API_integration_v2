d35d665a5a7857460fc403c8ccbc978d00c1592b
paul linden <paul.linden@gritacademy.se>
2023-09-14 11:36:04 +0200

    Added Gson to libs

--- app/src/main/java/com/three/apiintegration/fragments/SecondFragment.kt
+++ app/src/main/java/com/three/apiintegration/fragments/SecondFragment.kt
@@ -1,11 +1,11 @@
 package com.three.apiintegration.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.three.apiintegration.R
 
